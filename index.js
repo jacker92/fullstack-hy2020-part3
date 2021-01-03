c0bfc8f27a61e9d8d20c6a1aae5121041cf73e33
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-03 11:44:41 +0200

    Added task 3.9

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
-
+app.use(cors())
 app.use(express.json())
 
 let persons = [
