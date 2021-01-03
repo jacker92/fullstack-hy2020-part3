1babb2b3b82bfb806080932eeeb69b2a3794aaf3
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-03 10:51:17 +0200

    Added task 3.3

--- index.js
+++ index.js
@@ -20,11 +20,21 @@ app.get('/info', (req, res) => {
         Phonebook has info for ${persons.length} people
         </p>
         <p>${new Date()}</p>`
-        )
+    )
 })
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
