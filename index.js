39cbce54579f5926ed6fa6486d59c85007c05641
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-03 10:48:00 +0200

    Added task 3.2

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ let persons = [
     },
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>
+        Phonebook has info for ${persons.length} people
+        </p>
+        <p>${new Date()}</p>`
+        )
 })
 
 app.get('/api/persons', (req, res) => {
